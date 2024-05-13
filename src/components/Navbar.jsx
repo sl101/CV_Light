8eb48f4dfe65939db6f9431228adf49ee7df2a05
Slava <v.zhevaga@gmail.com>
2024-05-13 23:02:03 +0200

    fix logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import { NavLink } from "react-router-dom"
 const Navbar = () => {
 	return (
 		<header className="header">
-			<NavLink to="/" className="w-10 h-10 rounded-lg bg-white flex items-center justify-center font-bold shadow-md">
-				<p className="blue-gradient_text">VZ</p>
+			<NavLink to="/" className="py-1 px-2 rounded-lg bg-white flex items-center justify-center font-bold shadow-md">
+				<p className="blue-gradient_text">Slava Zh</p>
 			</NavLink>
 			<nav className="flex text-lg gap-7 font-medium">
-				<NavLink to="/about" className={({isActive})=> isActive ? 'text-blue-500' : 'text-black'}>About</NavLink>
-				<NavLink to="/projects" className={({isActive})=> isActive ? 'text-blue-500' : 'text-black'}>Project</NavLink>
-				<NavLink to="/contact" className={({isActive})=> isActive ? 'text-blue-500' : 'text-black'}>Contact</NavLink>
+				<NavLink to="/about" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>About</NavLink>
+				<NavLink to="/projects" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>Project</NavLink>
+				<NavLink to="/contact" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>Contact</NavLink>
 			</nav>
 		</header>
 	)
